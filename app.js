46790ab4cdbaf06c63be8d8b5aa237b80a4024ee
Martin Alexander Flores Torres <martinflorestorres21@gmail.com>
2022-07-07 22:44:08 -0500

    version 1

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
-const body = document.querySelector("body")
-const menu = document.querySelector("#menu")
+const body = document.querySelector("body");
+const menu = document.querySelector("#menu");
+const formulario = document.querySelector("#formulario");
 
-menu.addEventListener("click", (e) =>{
-  e.preventDefault()
-  body.classList.toggle("active")
-})
+menu.addEventListener("click", (e) => {
+  e.preventDefault();
+  body.classList.toggle("active");
+});
+
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const busquedad = document.querySelector("#busquedad").value;
+
+  if (busquedad.trim() != "") {
+    let url = `https://www.youtube.com/results?search_query=${busquedad}`;
+
+    const a = document.createElement("a");
+    a.href = url;
+    a.target = "_blank";
+    a.style.display = "none";
+    a.classList.add("linkBusquedad");
+    body.appendChild(a);
+    const link = document.querySelector(".linkBusquedad");
+    if (link) {
+      link.click();
+      link.remove();
+    }
+  } else {
+    formulario.classList.add("error");
+    setTimeout(() => {
+      if (formulario.classList.contains("error")) {
+        formulario.classList.remove("error");
+      }
+    }, 1000);
+    console.log("ee");
+  }
+});
